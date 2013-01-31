08be0612e82763788f2e2cd60b0cb788d8b3209c
rigges99 <rigges99@gmail.com>
2013-01-31 12:36:44 +0000

    Minor Update to Display Text

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello this is a gitbash test");
+            Console.WriteLine("Testing changes");
             Console.ReadLine();
         }
     }
